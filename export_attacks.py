e95aea2972d0a0ecc21ee8d92f94ca27ce8bb8ee
S0Cur1ty <50646204+S0Curity@users.noreply.github.com>
2024-01-09 14:15:16 +0800

    Create export_attacks.py

---
+++ export_attacks.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import sys
+import pandas as pd
+from argparse import ArgumentParser
+from dynatrace_api import DynatraceApi
+import logging
+import logging.config
+logging.basicConfig(filename='output.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# get the Dynatrace Environmemnt (URL) and the API Token with arguments
+# with the details parameter, the details for each security problem are fetched
+parser = ArgumentParser()
+parser.add_argument("-e", "--env", dest="environment", help="The Dynatrace Environment to query", required=True)
+parser.add_argument("-t", "--token", dest="token", help="The Dynatrace API Token to use", required=True)
+parser.add_argument("--debug", dest="debug", help="Set log level to debbug", action='store_true')
+
+parser.add_argument("-d", "--details", dest="details", help="Fetch the details for each security problem (takes longer)", action='store_true')
+parser.add_argument("-k", "--insecure", dest="insecure", help="Skip SSL certificate validation", action='store_true')
+
+args = parser.parse_args()
+
+env = args.environment
+apiToken = args.token
+showDetails = args.details
+verifySSL = not args.insecure
+
+debug = args.debug
+
+if debug:
+    logging.getLogger().setLevel(logging.DEBUG)
+
+logging.info("="*200)
+logging.info("Running %s ", " ".join(sys.argv))
+logging.info("="*200)
+
+
+def writeResultToFile(filename, result):
+    df = pd.json_normalize(result)
+    df.to_csv(filename,sep=';', index=False, quotechar="'", encoding='utf-8')
+    print()
+    print('results stored under a .csv file')
+
+dynatraceApi = DynatraceApi(env, apiToken, verifySSL)
+
+# retireve all security problems
+securityAttacks = dynatraceApi.getAttacks()
+
+# if the details flag is set, retrieve the details for every security problem
+# write result to a CSV file
+if showDetails:
+    securityAttackDetails = []
+    for secP in securityAttacks:
+        securityAttackDetail = dynatraceApi.getAttackDetails(secP["attackId"])
+        securityAttackDetails.append(securityAttackDetail)
+    writeResultToFile('securityAttackDetails.csv', securityAttackDetails)
+else:
+    writeResultToFile('securityAttacks.csv', securityAttacks)
